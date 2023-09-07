b63a86698bc5e8ec349da88fd02bcaeb985a0c23
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2023-09-07 22:04:52 +0100

    index update

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     // origin: "http:localhost:5001",
-    origin: "https://jobhubrest-production.up.railway.app",
+    origin: "jobhubserver-production.up.railway.app",
   },
 });
 
